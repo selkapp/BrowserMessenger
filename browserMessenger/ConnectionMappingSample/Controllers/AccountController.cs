cd791e14a247b2053f09b1eabc4e87aa68da0f13
selkapp <32335223+selkapp@users.noreply.github.com>
2018-04-27 16:37:04 +0300

    Y

---
+++ browserMessenger/ConnectionMappingSample/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using browserMessenger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace browserMessenger.Controllers {
+
+    public class AccountController : Controller {
+
+        public ViewResult Login() {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public ActionResult PostLogin(LoginModel loginModel) {
+
+            if (ModelState.IsValid) {
+
+                FormsAuthentication.SetAuthCookie(loginModel.Nick, true);
+                return RedirectToAction("index", "home");
+            }
+
+            return View(loginModel);
+        }
+
+        [HttpPost]
+        [ActionName("SignOut")]
+        public ActionResult PostSignOut() {
+
+            FormsAuthentication.SignOut();
+            return RedirectToAction("index", "home");
+        }
+    }
+}
---
+++ browserMessenger/ConnectionMappingSample/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace browserMessenger.Models {
+    
+    public class LoginModel {
+
+        [Required]
+        public string Nick { get; set; }
+    }
+}
